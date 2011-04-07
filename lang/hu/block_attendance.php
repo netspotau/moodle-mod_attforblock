ae03c271135a96a1e216c464049913491b779a32
Sergey Vidusov <sergey.vidusov@netspot.com.au>
2011-04-07 12:18:45 +0930

    Ported for Moodle 2.0.

---
+++ lang/hu/block_attendance.php
@@ -0,0 +1,9 @@
+<?PHP // $Id: block_attendance.php,v 1.1.2.1 2010/07/07 03:50:52 dlnsk Exp $ 
+      // block_attendance.php - created with Moodle 1.5.3+ (2005060230)
+
+
+$string['blockname'] = 'Katalógus';
+$string['needactivity'] = 'Ez a blokk csak a katalógus tevékenységgel együtt működik. Kérjük, adjon egy ilyen tevékenységet a kurzushoz.';
+$string['pluginname'] = 'Katalógus';
+
+?>
